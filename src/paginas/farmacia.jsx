963100509bb84e68e39906acd912a9ce7417a570
sofis08 <sofiavieracar08@gmail.com>
2025-09-15 16:22:31 -0500

    commit

--- src/paginas/farmacia.jsx
+++ src/paginas/farmacia.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "../css/farmacia.css";
 import "../css/logo.css";
 import { Link } from "react-router-dom";
+import pp from "../assets/img/acetam.png";
+import trime from "../assets/img/trimebt.png";
+import beclo from "../assets/img/beclomet.png";
+import Menu from "../componentes/menu";
+
 
 function Farmacia() {
   return (
     <div className="farmacia-container">
-      <Link to="/" className="logo-link">
-        <span className="logo"></span>
-        SaludVital
-      </Link>
-
-      {}
+      <Menu/>
       <nav className="search-navbar">
         <form className="search-form" role="search">
           <input
@@ -26,11 +26,52 @@ function Farmacia() {
         </form>
       </nav>
 
-      <h1  className="title-container"> <strong>Farmacia </strong></h1>
-       <div className="precauciones-container">
-            <h2></h2>
-            <p> Aquí encontrarás información sobre diversos medicamentos y sus precauciones</p>
+      <h1 className="title-container"> <strong>Farmacia </strong></h1>
+      <div className="container">
+        <div className="precauciones-container">
+          <p> Aquí encontrarás información sobre diversos medicamentos y sus precauciones</p>
+        </div>
+      </div>
+      <div id="carouselExampleCaptions" className="carousel slide">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={pp} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Acetaminofén</h5>
+              <p>Para niños: El acetaminofén ayuda a bajar la fiebre y aliviar dolores leves como los de cabeza o garganta. La dosis depende del peso y la edad, por eso siempre debe seguirse lo que indica el médico o el empaque. Es seguro si se usa correctamente, pero tomar demasiado puede afectar el hígado. No debe combinarse con otros medicamentos sin consultar al pediatra.</p> 
+
+             <p>Para adultos mayores: El acetaminofén se usa para aliviar dolores comunes y controlar la fiebre. Es más seguro que otros analgésicos para personas mayores, especialmente si tienen problemas de estómago. Sin embargo, deben evitarse las dosis altas y el uso prolongado, ya que puede dañar el hígado, sobre todo si se consume alcohol o hay enfermedades hepáticas. Siempre es mejor consultarlo con el médico.</p>
+            </div>
           </div>
+          <div className="carousel-item">
+            <img src={trime} className="d-block w-100" alt="..." />
+            <div className="carousel-caption">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={beclo} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -17,23 +17,23 @@ function Inicio() {
         <div className="container text-center">
           <div className="row">
             <div className="col col1">
-              ¿Por qué ejercitar mi cuerpo es bueno?
+              <strong>¿Por qué ejercitar mi cuerpo es bueno?</strong>
               <a> Ejercitarte mejora la salud, aumenta energía, fortalece músculos y previene enfermedades.</a>
             </div>
             <div className="col col2">
-              ¿Por qué comer bien es bueno para mi cuerpo?
+              <strong>¿Por qué comer bien es bueno para mi cuerpo?</strong>
               <a> Comer bien ayuda a tu energía y salud general.</a>
             </div>
             <div className="col col3">
-              ¿Por qué dormir bien ayuda a mi cuerpo?
+              <strong>¿Por qué dormir bien ayuda a mi cuerpo?</strong>
               <a> Dormir bien repara el cuerpo, mejora la memoria, fortalece el sistema inmunológico y regula el ánimo.</a>
             </div>
             <div className="col col4">
-              ¿Por qué tener una buena salud mental es importante?
+              <strong>¿Por qué tener una buena salud mental es importante?</strong>
               <a> Tener una buena salud mental mejora el bienestar, reduce el estrés, aumenta la felicidad y fortalece las relaciones.</a>
             </div>
             <div className="col col5">
-              ¿Por qué tener una relación sana con tus seres queridos es importante?
+              <strong>¿Por qué tener una relación sana con tus seres queridos es importante?</strong>
               <a> Tener una relación sana con tus seres queridos brinda apoyo emocional, fortalece la confianza y mejora el bienestar general.</a>
             </div>
           </div>
@@ -50,8 +50,8 @@ function Inicio() {
             <h3></h3>
             <p>Bienvenidos a nuestra pagina de salud general, diseñada para ofrecerte consejos practicos y recursos que te ayuden a mejorar tu bienestar físico y mental. Aquí encontrarás herramientas para vivir de forma más saludable y equilibrada.</p>
             <p> <strong> Contáctenos:Saludvital@gmail.com </strong></p>
-            <p>Teléfono: 314458799 </p>
-            <p>Instagram: @salud_vital </p>
+            <p><strong>Teléfono: 314458799</strong></p>
+            <p><strong>Instagram: @salud_vital</strong></p>
           </div>
         </div>
 
