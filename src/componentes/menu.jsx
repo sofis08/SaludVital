e47da9aef821a0fe4a4152083534378afe27c2b5
sofis08 <sofiavieracar08@gmail.com>
2025-10-01 16:09:01 -0500

    Farmacia nueva

--- src/componentes/menu.jsx
+++ src/componentes/menu.jsx
@@ -8,18 +8,16 @@ function Menu() {
     <div className="menu-container">
       <nav className="navbar navbar-light bg-white fixed-top border-bottom">
         <div className="container-fluid d-flex align-items-center">
-          { }
+
           <Link to='/'>
             <a
               className="navbar-brand fw-bold text-dark d-flex align-items-center me-3"
-              href="#"
             >
               <span className="logo me-2"></span>
               SaludVital
             </a>
           </Link>
 
-          { }
           <form className="d-flex flex-grow-1 mx-2" role="search">
             <input
               className="search-input form-control form-control-sm"
@@ -29,90 +27,79 @@ function Menu() {
             />
           </form>
 
-          { }
           <button
             className="navbar-toggler ms-2"
             type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          { }
           <div
-            className="offcanvas offcanvas-end text-bg-light"
-            tabIndex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title text-dark" id="offcanvasNavbarLabel">
-                Menú
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <Link to='/tumedicode24horas'>
-                    <a className="nav-link" aria-current="page" href="">
-                      Medico 24h
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/urgencias'>
-                    <a className="nav-link" href="#">
-                      Urgencias
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/farmacia'>
-                    <a className="nav-link" href="#">
-                      Farmacia
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/'>
-                    <a className="nav-link" href="#">
-                      Contactenos
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/perfil'>
-                    <a className="nav-link" href="#">
-                      Perfil
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/ajustes'>
-                    <a className="nav-link" href="#">
-                      Ajustes
-                    </a>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/iniciosesion'>
-                    <a className="nav-link" href="#">
-                      Cerrar Sesion
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            className="collapse navbar-collapse"
+            id="navbarNav">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              
+              <li className="nav-item">
+                <Link to='/tumedicode24horas'>
+                  <a className="nav-link" aria-current="page" href="#">
+                    Medico 24h
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/urgencias'>
+                  <a className="nav-link" href="#">
+                    Urgencias
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/farmacia'>
+                  <a className="nav-link" href="#">
+                    Farmacia
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/'>
+                  <a className="nav-link" href="#">
+                    Contactenos
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/perfil'>
+                  <a className="nav-link" href="#">
+                    Perfil
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/ajustes'>
+                  <a className="nav-link" href="#">
+                    Ajustes
+                  </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link to='/iniciosesion'>
+                  <a className="nav-link" href="#">
+                    Cerrar Sesion
+                  </a>
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
@@ -121,4 +108,3 @@ function Menu() {
 }
 
 export default Menu;
-
